34eb5744a2e7e27ba93f451a2ba71b2fc67d7740
Mithlesh yadav <93759352+Mithlesh-yadav@users.noreply.github.com>
2023-09-15 15:58:07 +0530

    Update 5.rightView.cpp

--- TREE/5.rightView.cpp
+++ TREE/5.rightView.cpp
@@ -1,1 +1,27 @@
-
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   vector<int>ans;
+   queue<Node*>q;
+   q.push(root);
+   if(root==NULL)return ans;
+   while(!q.empty()){
+       int val=0;
+       int size=q.size();
+       if(size==0)return ans;
+       while(size>0){
+           Node* temp=q.front();
+           q.pop();
+          val=temp->data;
+           if(temp->left!=NULL){
+               q.push(temp->left);
+           }
+           if(temp->right!=NULL){
+               q.push(temp->right);
+           }
+       }
+      ans.push_back(val);
+       
+   }
+   return ans;
+}
