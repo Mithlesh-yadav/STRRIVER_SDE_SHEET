dfc5fdce9c82b6eda7ea1776781e8faea256d24d
Mithlesh yadav <93759352+Mithlesh-yadav@users.noreply.github.com>
2023-10-03 15:53:56 +0530

    Create 18.Make_Mirror from binary tree.cpp

---
+++ TREE/18.Make_Mirror from binary tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void mirror(Node* node) {
+        // code here
+        if(node==NULL)return;
+        mirror(node->left);
+        mirror(node->right);
+        Node* temp=node->left;
+        node->left=node->right;
+        node->right=temp;
+    }
+};
